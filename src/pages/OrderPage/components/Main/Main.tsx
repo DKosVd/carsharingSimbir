9687a323d29d49c0b990f1cce5a4df1fcfd81266
DKosVd <dkosyhiyan@gmail.com>
2021-04-20 19:31:58 +0500

    add-main-layout

--- src/pages/OrderPage/components/Main/Main.tsx
+++ src/pages/OrderPage/components/Main/Main.tsx
@@ -6,6 +6,11 @@ import { Breadcrumbs } from '../Breadcrumbs/Breadcrumbs'
 
 export function Main() {
     const [open, setOpen] = React.useState<boolean>(false)
+    const [value, setValue] = React.useState<string>('default')
+
+    const handleChangeValue = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setValue(e.target.value)
+    }
 
     return (
         <>
@@ -14,13 +19,42 @@ export function Main() {
                 <div className="container">
                     <Header />
                 </div>
-                <Breadcrumbs/>
+                <Breadcrumbs />
                 <div className="order-main container">
                     <div className="order__chose__city">
+                        <div className="order__chose-form">
+                            <form className="order-form">
+                                <div className="order-form__elem">
+                                    <div className="optional-wrapper order-form__elem_layout select-close">
+                                        <label>Город</label>
+                                        <select value={value} onChange={handleChangeValue} className="underline order-form__elem_ml" >
+                                            <option value="default" disabled>Выберите город</option>
+                                            <option value="Samara">Самара</option>
+                                            <option value="Ul'anovsk">Ульяновск</option>
+                                            <option value="Sizran'">Сызрань</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="order-form__elem ">
+                                    <div className="optional-wrapper order-form__elem_layout" >
+                                        <label>Пункт выдачи</label>
+                                        <input type="text" placeholder="Начните вводить пункт..." className="underline order-form__elem_ml" />
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="order-city">
+                            <p>Выбрать на карте</p>
+                            <div className="order__city__img">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="order__empty">
 
                     </div>
                     <div className="order__yours__order">
-                        
+
                     </div>
                 </div>
             </div>
