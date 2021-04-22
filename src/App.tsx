7477f8ba5524d3830e69186d2289b3b2467789ff
DKosVd <dkosyhiyan@gmail.com>
2021-04-22 22:10:27 +0500

    fix-e-mp

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import { MainPage } from './pages/MainPage/MainPage';
 import { OrderPage } from './pages/OrderPage/OrderPage';
 
 function App() {
   return (
     <>
-      {/* <MainPage/> */}
-      <OrderPage/>
+      <MainPage/>
+      {/* <OrderPage/> */}
     </>
   );
 }
