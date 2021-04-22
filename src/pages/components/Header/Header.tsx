ab4a9426b35716802ce3264ae73c2bfe30955c2b
DKosVd <dkosyhiyan@gmail.com>
2021-04-22 21:58:48 +0500

    fix-error

--- src/pages/components/Header/Header.tsx
+++ src/pages/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../../../../styles/header.css'
+import '../../../styles/header.css'
 import { Map } from "@styled-icons/boxicons-regular/Map"
 
 export default function Header() {
