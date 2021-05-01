ffe9a7d03f21636d965a6734658a0aaa4c6e9df6
DKosVd <dkosyhiyan@gmail.com>
2021-05-01 14:53:13 +0500

    v02-v03

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { store } from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/">
       <Provider store={store}>
         <App />
       </Provider>
