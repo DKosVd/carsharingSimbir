b5c2d618b3f9a1c3f26fa6ed82cc7cd3e8c0c4cf
DKosVd <dkosyhiyan@gmail.com>
2021-05-05 16:03:22 +0500

    v02-v03_f02-f03

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 axios.interceptors.request.use((config) => {
-    config.headers['X-Api-Factory-Application-Id'] = process.env.REACT_APP_APLICATION_ID
+    config.headers['X-Api-Factory-Application-Id'] = process.env.REACT_APP_APLICATION_ID || "5e25c641099b810b946c5d5b"
     return config;
 })
 
